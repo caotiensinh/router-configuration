42017b580d5814161becea879246fc2dbae2db85
Mr.Cao <cdn140493@gmail.com>
2026-09-02 14:28:24 +0900

    test(routeros): lock routing table cli surface

--- tests/test_routeros_routing_table_syntax.py
+++ tests/test_routeros_routing_table_syntax.py
@@ -4,7 +4,7 @@ from router_configuration.routeros_renderer import RouterOSSafeSubsetRenderer
 
 
 class RouterOSRoutingTableSyntaxTests(unittest.TestCase):
-    def test_routing_table_create_uses_explicit_fib_yes(self):
+    def test_routing_table_command_uses_supported_cli_surface_only(self):
         command = RouterOSSafeSubsetRenderer()._ensure_routing_table_command(
             command_id="route.00.table.test",
             operation_id="routing.multiwan.capacity_weighted",
@@ -12,9 +12,10 @@ class RouterOSRoutingTableSyntaxTests(unittest.TestCase):
             comment="routercfg:managed:routing-table:wan10g",
             risk=30,
         ).command
-        self.assertIn("/routing/table/add fib=yes ", command)
-        self.assertNotIn("/routing/table/add fib ", command)
-        self.assertIn("/routing/table/set $rid fib=yes ", command)
+        self.assertIn('/routing/table/add name="to-wan10g" fib', command)
+        self.assertIn("/routing/table/set $rid fib=yes", command)
+        self.assertNotIn("comment=", command)
+        self.assertNotIn("/routing/table/add fib=yes", command)
 
 
 if __name__ == "__main__":
