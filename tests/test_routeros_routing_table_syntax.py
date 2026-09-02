21df7b2dfbea3d2ec037c9594e51ed87b8211b97
Mr.Cao <cdn140493@gmail.com>
2026-09-02 14:51:11 +0900

    test(routeros): forbid routing table flag mutation

--- tests/test_routeros_routing_table_syntax.py
+++ tests/test_routeros_routing_table_syntax.py
@@ -4,16 +4,15 @@ from router_configuration.routeros_renderer import RouterOSSafeSubsetRenderer
 
 
 class RouterOSRoutingTableSyntaxTests(unittest.TestCase):
-    def test_routing_table_command_uses_supported_cli_surface_only(self):
+    def test_routing_table_command_only_creates_missing_fib_table(self):
         command = RouterOSSafeSubsetRenderer()._ensure_routing_table_command(
             command_id="route.00.table.test",
             operation_id="routing.multiwan.capacity_weighted",
             table="to-wan10g",
-            comment="routercfg:managed:routing-table:wan10g",
             risk=30,
         ).command
         self.assertIn('/routing/table/add name="to-wan10g" fib', command)
-        self.assertIn("/routing/table/set $rid fib=yes", command)
+        self.assertNotIn("/routing/table/set", command)
         self.assertNotIn("comment=", command)
         self.assertNotIn("/routing/table/add fib=yes", command)
 
